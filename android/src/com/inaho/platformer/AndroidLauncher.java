cb61e05a998d48f309f33adef3243b8dcb183fbd
Inaho <inahoyuhara@gmail.com>
2017-02-13 18:12:15 -0500

    Ajout du projet libGDX de base

---
+++ android/src/com/inaho/platformer/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.inaho.platformer;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.inaho.platformer.MyPlatformer;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyPlatformer(), config);
+	}
+}
