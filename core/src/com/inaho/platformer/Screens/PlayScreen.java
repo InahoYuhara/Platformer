367059be872715b4680d4828c49620248f8c919b
Inaho <inahoyuhara@gmail.com>
2017-02-13 18:58:33 -0500

    Creation de la classe PlayScreen

---
+++ core/src/com/inaho/platformer/Screens/PlayScreen.java
@@ -0,0 +1,66 @@
+package com.inaho.platformer.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.inaho.platformer.MyPlatformer;
+
+/**
+ * Created by inaho on 2017-02-13.
+ */
+
+public class PlayScreen implements Screen{
+
+    private MyPlatformer game;
+    Texture texture;
+
+
+
+    public PlayScreen(MyPlatformer game){
+        this.game = game;
+        texture = new Texture("badlogic.jpg");
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.begin();
+        game.batch.draw(texture,0,0);
+        game.batch.end();
+
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        game.batch.dispose();
+
+    }
+}
