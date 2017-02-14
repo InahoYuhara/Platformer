e2a135832a84ea500f2b4d9e1756f89c54fb5cf0
InahoYuhara <inahoyuhara@gmail.com>
2017-02-14 00:40:24 -0500

    ajout des collisions. 'mario' peut sauter. Mouvement tres lent pour le moment

--- core/src/com/mygdx/game/Screens/PlayScreen.java
+++ core/src/com/mygdx/game/Screens/PlayScreen.java
@@ -1,19 +1,31 @@
 package com.mygdx.game.Screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.Platformer;
 import com.mygdx.game.Scenes.Hud;
+import com.mygdx.game.Sprites.Mario;
 
 /**
  * Created by antoine on 2017-02-13.
@@ -27,9 +39,17 @@ public class PlayScreen implements Screen {
     private Viewport gamePort;
     private Hud hud;
 
+    //Tiled map Variables
     private TmxMapLoader mapLoader;
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
+
+    //Box2D variables
+    private World world;
+    private Box2DDebugRenderer b2dr;
+    private Mario player;
+
+
 
     public PlayScreen(Platformer game){
         this.game = game;
@@ -41,7 +61,77 @@ public class PlayScreen implements Screen {
         mapLoader =  new TmxMapLoader();
         map = mapLoader.load("testMap.tmx");
         renderer = new OrthogonalTiledMapRenderer(map);
+
         gameCam.position.set(gamePort.getWorldWidth() / 2,gamePort.getWorldHeight() / 2, 0);
+
+        world = new World(new Vector2(0, -10), true);
+        b2dr = new Box2DDebugRenderer();
+
+        player = new Mario(world);
+
+        BodyDef bdef = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fdef = new FixtureDef();
+        Body body;
+
+
+
+        //create ground bodies/fixtures
+        for(MapObject object : map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set((rect.getX() + rect.getWidth() / 2), (rect.getY() + rect.getHeight() / 2));
+
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2 , rect.getHeight() / 2);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+
+        }
+        //create pipe bodies/fixtures
+        for(MapObject object : map.getLayers().get(4).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set((rect.getX() + rect.getWidth() / 2), (rect.getY() + rect.getHeight() / 2));
+
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2 , rect.getHeight() / 2);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+
+        }
+        //create brick bodies/fixtures
+        for(MapObject object : map.getLayers().get(5).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set((rect.getX() + rect.getWidth() / 2), (rect.getY() + rect.getHeight() / 2));
+
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2 , rect.getHeight() / 2);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+
+        }
+        //create coin bodies/fixtures
+        for(MapObject object : map.getLayers().get(3).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set((rect.getX() + rect.getWidth() / 2), (rect.getY() + rect.getHeight() / 2));
+
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2 , rect.getHeight() / 2);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+
+        }
 
     }
 
@@ -53,13 +143,15 @@ public class PlayScreen implements Screen {
 
     public void HandleInput(float delta){
 
-        if(Gdx.input.isTouched()){
-            gameCam.position.x += 100 * delta;
+        if(Gdx.input.isKeyJustPressed(Input.Keys.UP)){
+            player.b2Body.applyLinearImpulse(new Vector2(0,16f),player.b2Body.getWorldCenter(), true);
         }
 
     }
     public void update(float delta){
         HandleInput(delta);
+
+        world.step(1/60f, 6, 2);
 
         gameCam.update();
         renderer.setView(gameCam);
@@ -68,12 +160,20 @@ public class PlayScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        //separate update logic from render
         update(delta);
 
+        //clear the game screen with black
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        //render game map
         renderer.render();
+
+        //render Box2DDebugLines
+        b2dr.render(world,gameCam.combined);
+
+        //Set the batch to draw what the Hud camera sees
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
 
