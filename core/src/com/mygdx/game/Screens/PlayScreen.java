cf80bfd4a06724c1ee0df52a566dfb7a911a76b9
InahoYuhara <inahoyuhara@gmail.com>
2017-02-13 23:28:28 -0500

    Ajout de la map. Click to sidescroll function.

--- core/src/com/mygdx/game/Screens/PlayScreen.java
+++ core/src/com/mygdx/game/Screens/PlayScreen.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
@@ -24,6 +27,10 @@ public class PlayScreen implements Screen {
     private Viewport gamePort;
     private Hud hud;
 
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+
     public PlayScreen(Platformer game){
         this.game = game;
 
@@ -31,6 +38,10 @@ public class PlayScreen implements Screen {
         gamePort = new FitViewport(Platformer.V_WIDTH,Platformer.V_HEIGHT,gameCam);
         hud = new Hud(game.batch);
 
+        mapLoader =  new TmxMapLoader();
+        map = mapLoader.load("testMap.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        gameCam.position.set(gamePort.getWorldWidth() / 2,gamePort.getWorldHeight() / 2, 0);
 
     }
 
@@ -40,11 +51,29 @@ public class PlayScreen implements Screen {
 
     }
 
+    public void HandleInput(float delta){
+
+        if(Gdx.input.isTouched()){
+            gameCam.position.x += 100 * delta;
+        }
+
+    }
+    public void update(float delta){
+        HandleInput(delta);
+
+        gameCam.update();
+        renderer.setView(gameCam);
+
+    }
+
     @Override
     public void render(float delta) {
+        update(delta);
+
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        renderer.render();
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
 
