6d1f809c19152f7b43d9ccf6a5b7d8a11e853f00
InahoYuhara <inahoyuhara@gmail.com>
2017-02-13 21:22:25 -0500

    Ajout du stage

--- core/src/com/mygdx/game/Screens/PlayScreen.java
+++ core/src/com/mygdx/game/Screens/PlayScreen.java
@@ -3,8 +3,14 @@ package com.mygdx.game.Screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.Platformer;
+import com.mygdx.game.Scenes.Hud;
 
 /**
  * Created by antoine on 2017-02-13.
@@ -13,11 +19,17 @@ import com.mygdx.game.Platformer;
 public class PlayScreen implements Screen {
 
     private Platformer game;
-    Texture texture;
+
+    private OrthographicCamera gameCam;
+    private Viewport gamePort;
+    private Hud hud;
 
     public PlayScreen(Platformer game){
         this.game = game;
-        texture = new Texture("badlogic.jpg");
+
+        gameCam = new OrthographicCamera();
+        gamePort = new FitViewport(Platformer.V_WIDTH,Platformer.V_HEIGHT,gameCam);
+        hud = new Hud(game.batch);
 
 
     }
@@ -30,16 +42,17 @@ public class PlayScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        game.batch.begin();
-        game.batch.draw(texture, 0, 0);
-        game.batch.end();
+
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
+
     }
 
     @Override
     public void resize(int width, int height) {
-
+        gamePort.update(width,height);
     }
 
     @Override
