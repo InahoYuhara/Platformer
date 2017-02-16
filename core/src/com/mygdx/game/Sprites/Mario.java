a8e2a111f3630829093eacdbe771595cdbc1197a
Inaho <inahoyuhara@gmail.com>
2017-02-15 19:25:37 -0500

    Animation de mario. STAND,JUMP,RUN,FALL

--- core/src/com/mygdx/game/Sprites/Mario.java
+++ core/src/com/mygdx/game/Sprites/Mario.java
@@ -1,5 +1,6 @@
 package com.mygdx.game.Sprites;
 
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.physics.box2d.Body;
@@ -7,6 +8,7 @@ import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
 import com.mygdx.game.Platformer;
 import com.mygdx.game.Screens.PlayScreen;
 
@@ -15,15 +17,46 @@ import com.mygdx.game.Screens.PlayScreen;
  */
 
 public class Mario extends Sprite {
+    public enum State {FALLING, JUMPING, STANDING, RUNNING};
+    public State currentState;
+    public State previousState;
+
     public World world;
     public Body b2Body;
+
     private TextureRegion marioStand;
 
+    private Animation marioRun;
+    private Animation marioJump;
+    private float stateTimer;
+    private boolean runningRight;
+
+
     public Mario(World world, PlayScreen screen){
+
         super(screen.getAtlas().findRegion("little_mario"));
         this.world = world;
-        defineMario();
+        currentState = State.STANDING;
+        previousState = State.STANDING;
+        stateTimer = 0;
+        runningRight = true;
+
+        Array<TextureRegion> frames = new Array<TextureRegion>();
+        for(int i = 1; i < 4; i++)
+        {
+            frames.add(new TextureRegion(getTexture(), i * 16, 0, 16, 16));
+        }
+        marioRun = new Animation(0.1f, frames);
+        frames.clear();
+        for(int i = 4; i < 6; i++)
+        {
+            frames.add(new TextureRegion(getTexture(), i * 16, 0, 16, 16));
+        }
+        marioJump = new Animation(0.1f, frames);
+
         marioStand = new TextureRegion(getTexture(), 0, 0, 16, 16);
+
+        defineMario();
         setBounds(0, 0, 16/Platformer.PPM, 16/Platformer.PPM);
         setRegion(marioStand);
 
@@ -32,7 +65,50 @@ public class Mario extends Sprite {
 
     public void update(float delta){
         setPosition(b2Body.getPosition().x - getWidth() / 2, b2Body.getPosition().y - getHeight() / 2);
+        setRegion(getFrame(delta));
 
+    }
+
+    public TextureRegion getFrame(float delta){
+        currentState = getState();
+
+        TextureRegion region;
+        switch (currentState)
+        {
+            case JUMPING:
+                region = (TextureRegion) marioJump.getKeyFrame(stateTimer);
+                break;
+            case RUNNING:
+                region = (TextureRegion) marioRun.getKeyFrame(stateTimer, true);
+                break;
+            case FALLING:
+            case STANDING:
+            default:
+                region = marioStand;
+                break;
+        }
+        if((b2Body.getLinearVelocity().x < 0 || !runningRight) && !region.isFlipX()){
+            region.flip(true,false);
+            runningRight = false;
+        }
+        else if((b2Body.getLinearVelocity().x > 0 || runningRight) && region.isFlipX()){
+            region.flip(true,false);
+            runningRight = true;
+        }
+
+        stateTimer = currentState == previousState ? stateTimer + delta : 0;
+        previousState = currentState;
+        return region;
+
+    }
+    public State getState(){
+        if(b2Body.getLinearVelocity().y > 0 || (b2Body.getLinearVelocity().y < 0 && previousState == State.JUMPING))
+            return State.JUMPING;
+        else if(b2Body.getLinearVelocity().y < 0)
+            return State.FALLING;
+        else if(b2Body.getLinearVelocity().x != 0)
+            return State.RUNNING;
+        else return State.STANDING;
     }
 
     public void defineMario(){
@@ -43,7 +119,7 @@ public class Mario extends Sprite {
 
         FixtureDef fdef = new FixtureDef();
         CircleShape shape = new CircleShape();
-        shape.setRadius(5/Platformer.PPM);
+        shape.setRadius(6/Platformer.PPM);
 
         fdef.shape = shape;
         b2Body.createFixture(fdef);
